655aac4af64187e73c1dedb5c844482a63b3bb7e
maokebing <kebing1011@163.com>
2016-05-17 23:23:17 +0800

    add framework target

---
+++ YFAudio/YFAudio.h
@@ -0,0 +1,13 @@
+
+/***********************************************************
+ //  YFAudio.h
+ //  Mao Kebing
+ //  Created by mac on 13-7-25.
+ //  Copyright (c) 2013 Eduapp. All rights reserved.
+ ***********************************************************/
+
+
+
+#import <YFAudio/YFAudioManager.h>
+#import <YFAudio/YFAudioPlayer.h>
+#import <YFAudio/YFAudioRecorder.h>
